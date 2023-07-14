4782c29adc1d6a715e5edbaf7222dbbb582acc89
Youness Dominique <youness.dominique@gmail.com>
2023-07-14 12:18:05 +0200

    [FEAT] Create auth field input text

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:twitterclone/constants/ui_constants.dart';
+import 'package:twitterclone/features/auth/widgets/auth_field.dart';
 
 class LoginView extends StatefulWidget{
   const LoginView ({super.key});
@@ -10,11 +11,39 @@ class LoginView extends StatefulWidget{
 
 class _LoginViewState extends State<LoginView>{
   final appBar = UIConstants.appBar();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
 
   @override
   Widget build (BuildContext context){
     return Scaffold(
       appBar: appBar,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              
+              AuthField(
+                controller: emailController,
+                hintText: 'Email',
+              ),
+              
+              AuthField(
+                controller: passwordController,
+                hintText: 'Password',
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
