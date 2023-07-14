c4e5bbc638b312473674f60c43b507f441be8f60
Youness Dominique <youness.dominique@gmail.com>
2023-07-14 11:18:31 +0200

    [FEAT] Appwrite configuration

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '64b1120116114d0083ce';
+  static const String projectId = '64ae8a80d1a0ca8def93';
+  static const String endPoint = 'https://cloud.appwrite.io/v1';
+}
