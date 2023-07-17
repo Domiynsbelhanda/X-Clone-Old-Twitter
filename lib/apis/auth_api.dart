38d759968fb1c47a19214abd296982f914b39821
Youness Dominique <youness.dominique@gmail.com>
2023-07-17 13:37:14 +0200

    [FEAT] Function auth api signup

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,9 +1,35 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
 import 'package:twitterclone/core/core.dart';
+import 'package:fpdart/fpdart.dart';
 
 abstract class IAuthAPI{
-  FutureEither<Account> signUp({
+  FutureEither<model.Account> signUp({
     required String email,
     required String password
   });
+}
+
+class AuthAPI implements IAuthAPI{
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+
+  @override
+  FutureEither<model.Account> signUp({
+    required String email,
+    required String password}) async {
+    try{
+      final account = await _account.create(
+          userId: ID.unique(),
+          email: email,
+          password: password
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace){
+      return left(Failure(e.message??'Some unexpected error occured.', stackTrace));
+    }
+    catch (e, stackTrace){
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
 }
