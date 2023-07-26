cd55177e3c094be176c45de567fd553f8c8d7192
Youness Dominique <youness.dominique@gmail.com>
2023-07-26 12:50:50 +0200

    style: add cupertinotabbar in homeview

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,14 +1,67 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:twitterclone/constants/constants.dart';
+import 'package:twitterclone/theme/theme.dart';
 
-class HomeView extends ConsumerWidget{
+class HomeView extends StatefulWidget{
   static route() => MaterialPageRoute(
     builder: (context) => const HomeView(),
   );
   const HomeView({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeViewState();
+  }
+}
+
+class _HomeViewState extends State<HomeView>{
+  int _page = 0;
+  final appBar = UIConstants.appBar();
+
+  void onPageChange(int index){
+    setState(() {
+      _page = index;
+    });
+  }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      body: IndexedStack(
+        index: _page,
+        children: [],
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: _page,
+        onTap: onPageChange,
+        backgroundColor: Pallete.backgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              _page == 0 ?
+              AssetsConstants.homeFilledIcon : AssetsConstants.homeOutlinedIcon,
+              color: Pallete.whiteColor,
+            )
+          ),
+
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AssetsConstants.searchIcon,
+                color: Pallete.whiteColor,
+              )
+          ),
+
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                _page == 2 ?
+                AssetsConstants.notifFilledIcon : AssetsConstants.notifOutlinedIcon,
+                color: Pallete.whiteColor,
+              )
+          )
+        ],
+      ),
+    );
   }
 }
